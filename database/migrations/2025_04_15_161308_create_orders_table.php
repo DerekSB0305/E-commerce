419dddfe387c812da2a455a1270758f26ab59c5b
DerekSB0305 <dereksolorzano2111@gmail.com>
2025-04-15 22:00:12 -0600

    Tabla Ordenes

--- database/migrations/2025_04_15_161308_create_orders_table.php
+++ database/migrations/2025_04_15_161308_create_orders_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id');
+            $table->timestamps('order_date');
+            $table->string('status');
         });
     }
 
