91187496ce192150756ba37e7118fbdbcacbdbb0
DerekSB0305 <dereksolorzano2111@gmail.com>
2025-04-16 11:12:45 -0600

    Tabla Categoria Producto

--- database/migrations/2025_04_15_161734_create_product_categories_table.php
+++ database/migrations/2025_04_15_161734_create_product_categories_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('product_categories', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('product_id');
+            $table->foreignId('category_id');
             $table->timestamps();
         });
     }
