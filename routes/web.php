80aebf401bb600de25db8533dd6a4a2764762751
DerekSB0305 <dereksolorzano2111@gmail.com>
2025-04-19 20:15:32 -0600

    Creación de los controladores

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,39 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\OrderItemController;
+use App\Http\Controllers\Admin\PaymentController;
+use App\Http\Controllers\Admin\ProductCategoryController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Prueba de relaciones
-use App\Models\Order;
+// Ruta de prueba para verificar las relaciones entre modelos
+// (Borrar al finalizar las pruebas para evitar filtrado de datos sensibles)
+ use App\Models\Order;
 
-Route::get('/test', function () {
-    $order = Order::with(['user', 'orderItems.product', 'payments'])->first();
-    return response()->json($order);
-});
-
+ Route::get('/test', function () {
+     $order = Order::with(['user', 'orderItems.product', 'payments'])->first();
+     return response()->json($order);
+ });
 // 
+
+// Acceso a los controladores de tipo Resource
+Route::resource('orders', OrderController::class);
+
+Route::resource('order-items', OrderItemController::class);
+
+Route::resource('products', ProductController::class);
+
+Route::resource('categories', CategoryController::class);
+
+Route::resource('product-categories', ProductCategoryController::class);
+
+Route::resource('payments', PaymentController::class);
+//
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
